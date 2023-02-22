2690fae13b023ac97c4515ad93a8c2f3b087a0bf
mukulembezewil <wilmukulembeze@gmail.com>
2023-02-22 03:53:02 +0300

    Green: fibonacci(0) passed test

---
+++ fibonacci.js
@@ -0,0 +1,22 @@
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  }
+
+  let prev = 0;
+  let curr = 1;
+  for (let i = 2; i <= n; i++) {
+    const next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+
+  return curr;
+}
+
+// Returns sequence to 10th position
+// for (let i = 0; i < 10; i++) {
+//   console.log(fibonacci(i));
+// }
+
+module.exports = fibonacci;
