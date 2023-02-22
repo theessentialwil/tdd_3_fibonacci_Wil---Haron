ac074668b9985bff4580ca1683c7efe75a252940
mukulembezewil <wilmukulembeze@gmail.com>
2023-02-22 03:59:32 +0300

    Green passed two more tests

--- test/testfibonacci.js
+++ test/testfibonacci.js
@@ -3,6 +3,14 @@ const fibonacci = require("../fibonacci");
 
 describe("Fibonacci test-first tests", () => {
   it("fibonacci(0) == 0", () => {
-    assert.equal(fibonacci(10), 55);
+    assert.equal(fibonacci(0), 0);
+  });
+
+  it("fibonacci(2) == 1", () => {
+    assert.equal(fibonacci(2), 1);
+  });
+
+  it("fibonacci(3) == 2", () => {
+    assert.equal(fibonacci(3), 2);
   });
 });
